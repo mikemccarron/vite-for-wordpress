b269c911b070350a550895401266840dad6e9de7
Mike McCarron <mikemccarron@1trickpony.com>
2024-07-17 14:47:42 -0400

    Updated initial PHP

--- dist/viteforwordpress.php
+++ dist/viteforwordpress.php
@@ -1,1 +1,10 @@
-<?php echo ""; ?>
+<?php
+    /*
+    Plugin Name: 1 Trick Pony - Vite for WordPress
+    Plugin URI: 1 Trick Pony
+    description: Handles HMR in WordPress.
+    Author: 1 Trick Pony
+    Version: 1.0.0
+    Author URI: https://1trickpony.com
+    */
+?>
