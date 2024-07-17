b8d7e319f947cd2742545cfc59cf391a16b3c8be
Mike McCarron <mikemccarron@1trickpony.com>
2024-07-17 16:28:08 -0400

    Updated for better Composer support

--- dist/viteforwordpress.php
+++ dist/viteforwordpress.php
@@ -1,10 +1,13 @@
 <?php
-    /*
-    Plugin Name: 1 Trick Pony - Vite for WordPress
-    Plugin URI: 1 Trick Pony
-    description: Handles HMR in WordPress.
-    Author: 1 Trick Pony
-    Version: 1.0.0
-    Author URI: https://1trickpony.com
-    */
+    /**
+     * Plugin Name: 1 Trick Pony - Vite for WordPress
+     * Description: Handles HMR in WordPress.
+     * Version: 1.0.0
+     * Author: 1 Trick Pony
+     * Author URI: https://1trickpony.com
+     * License: MIT
+     */
+
+    // Load the Composer autoloader
+    require_once __DIR__ . '/vendor/autoload.php';
 ?>
